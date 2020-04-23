cd4bfb6638357a9d778be67496297c3043242e56
Jason W. Clary <jwclary411@gmail.com>
2020-04-23 15:05:15 -0400

    Created Account class

---
+++ Project0/src/com/revature/beans/Account.java
@@ -0,0 +1,39 @@
+package com.revature.beans;
+
+public class Account {
+	
+// Class variables
+	private String accOwner; // Stores owners name
+	private int accNum;	     // Stores owners account number
+	private double accBal;   // Stores owners account balance
+	public boolean accActive;// checks to see if account is active
+	
+	public Account() {
+		
+	}
+
+// Getters & Setters
+	public String getAccOwner() {
+		return accOwner;
+	}
+
+	public void setAccOwner(String accOwner) {
+		this.accOwner = accOwner;
+	}
+
+	public int getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(int accNum) {
+		this.accNum = accNum;
+	}
+
+	public double getAccBal() {
+		return accBal;
+	}
+
+	public void setAccBal(double accBal) {
+		this.accBal = accBal;
+	}
+}
