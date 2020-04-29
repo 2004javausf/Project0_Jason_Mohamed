9a845f4538c592e5dfa81af8bb3a687be1d01f79
Mohamed Samatar <mohamedsamatar100@gmail.com>
2020-04-29 09:41:54 -0700

    Validation

--- Project0/src/com/revature/beans/User.java
+++ Project0/src/com/revature/beans/User.java
@@ -65,34 +65,4 @@ public class User implements Serializable{
 		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", name=" + name
 				+ ", bankAcct=" + bankAcct + "]";
 	}
-
-	//Registers user and adds them to account list
-	public static User register(String username, String password, String name) {
-		return new User(username, password, name);
-	}
-	
-	public static User login(String username, String password, ArrayList<User> userList) {
-		Iterator<User> it = userList.iterator();
-		
-		//return the user that matches login if it exists
-		while(it.hasNext()) {
-			User u = it.next();
-			if(u.username.equalsIgnoreCase(username) && u.password.equals(password))
-				return u;
-		}
-		
-		//if not found:
-		return null;
-	}
-
-	public void saveData(ArrayList<User> userList) throws IOException {
-		OutputStream os = new FileOutputStream("users.txt");
-		
-		//output user data to file in the format defined in toString()
-		for(User u : userList) {
-			String userString = u.toString() + "\n";
-			os.write(userString.getBytes());
-		}
-		os.close();
-	}
 }
--- Project0/src/com/revature/validation/Validate.java
+++ Project0/src/com/revature/validation/Validate.java
@@ -1,26 +1,66 @@
 package com.revature.validation;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+
+import com.revature.beans.User;
 
 public class Validate {
 	
 	// Scanner to take in user input
 	public static Scanner userInput = new Scanner(System.in);
 
-//Checks the user input and will try and parse it to an int
-public static int CheckInt(String input, String errorMessage) {
-	try {
-		return Integer.parseInt(input);		
-	}
-	catch (NumberFormatException e) {
-		System.out.println(errorMessage);
+	//Checks the user input and will try and parse it to an int
+	public static int CheckInt(String input, String errorMessage) {
+		try {
+			return Integer.parseInt(input);		
+		}
+		catch (NumberFormatException e) {
+			System.out.println(errorMessage);
+			
+			// New user input and reset prompts
+			String newInput = userInput.nextLine();
+			CheckInt(newInput, errorMessage);
+		}	
 		
-		// New user input and reset prompts
-		String newInput = userInput.nextLine();
-		CheckInt(newInput, errorMessage);
-	}	
+		return 0;
+	}
 	
-	return 0;
-}
+	//Registers user and adds them to account list
+	public static boolean register(String username, String password, String name, ArrayList<User> usersList) {		
+		for(User user : usersList)
+			if(username.equalsIgnoreCase(user.getUsername())) {
+				//System.out.println("username already exists");
+				return false;
+			}
+		
+		usersList.add(new User(username, password, name));
+		return true;
+	}
+	
+	public static User login(String username, String password, ArrayList<User> usersList) {
+		Iterator<User> it = usersList.iterator();
+		
+		//return the user that matches login if it exists
+		while(it.hasNext()) {
+			User u = it.next();
+			if(u.getUsername().equalsIgnoreCase(username) && u.getPassword().equals(password))
+				return u;
+		}
+		
+		//if not found:
+		return null;
+	}
+
+	public void saveData(ArrayList<User> userList) throws IOException {
+		ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream("users.txt"));
+		
+		objectOut.writeObject(userList);
+		objectOut.close();
+	}
 
 }
